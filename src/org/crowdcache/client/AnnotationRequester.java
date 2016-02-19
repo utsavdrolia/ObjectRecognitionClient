fab2ddebb297efe68fec87dc77d9b5c8f006ccf9
Utsav Drolia <utsavdrolia@gmail.com>
2016-02-18 22:00:35 -0500

    Now only one-time allocation of buffer and then reuse.

--- src/org/crowdcache/client/AnnotationRequester.java
+++ src/org/crowdcache/client/AnnotationRequester.java
@@ -7,11 +7,13 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
+
 /**
  * Created by utsav on 2/9/16.
  */
 public class AnnotationRequester extends Client
 {
+    ByteBuffer buffer = ByteBuffer.allocate(80 * 1024);
     /**
      * Address to connect to
      *
@@ -30,16 +32,16 @@ public class AnnotationRequester extends Client
      */
     public String requestAnnotation(String imagepath) throws IOException
     {
+        buffer.clear();
         FileInputStream fis = new FileInputStream(imagepath);
         FileChannel fc = fis.getChannel();
-        ByteBuffer buffer = ByteBuffer.allocate((int) (fc.size()));
         fc.read(buffer);
         buffer.flip();
+        fc.close();
         byte[] data = buffer.array();
         System.out.println("Requesting");
         return new String(request(data), ZMQ.CHARSET);
     }
-
 
     public static void main(String args[]) throws IOException
     {
